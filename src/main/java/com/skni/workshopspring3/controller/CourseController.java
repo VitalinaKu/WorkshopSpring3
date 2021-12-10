1465663fc29070b2cccd8d2020e5ca410b2d4f0f
VitalinaKu <vitalina.photo@gmail.com>
2021-12-10 18:44:58 +0100

    Added handleValidationExceptions. The task is done

--- src/main/java/com/skni/workshopspring3/controller/CourseController.java
+++ src/main/java/com/skni/workshopspring3/controller/CourseController.java
@@ -5,8 +5,13 @@ import com.skni.workshopspring3.service.CourseService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -41,7 +46,20 @@ public class CourseController {
     }
 
     @DeleteMapping("/course/{name}")
-        public ResponseEntity<?> deleteCourseByName(@PathVariable String name) {
+    public ResponseEntity<?> deleteCourseByName(@PathVariable String name) {
         return courseService.deleteCourseByName(name) ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
